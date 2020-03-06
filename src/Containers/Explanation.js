82ff726526c91f3fb2343666589568530a28a36f
yang <yueyangyu42@gmail.com>
2020-03-06 13:44:07 -0800

    Update explanation styles

--- src/Containers/Explanation.js
+++ src/Containers/Explanation.js
@@ -7,8 +7,8 @@ const Explanation = () => {
   return (
     <Column stylesClass="explanation">
       <Text stylesClass="explanation-title" text="How it works!" />
-      <Text text="1. Upload any number of images of anyone with their face clearly in view." />
-      <Text text="2. Generate the emojis." />
+      <Text text="1. Upload any number of images with faces clearly in view." />
+      <Text text="2. Generate all the  emojis with one click." />
       <Text text="3. Save them all as a zip with one click!" />
     </Column>
   );
