e36828c9c207ecc38b22651e6e0f75cb5e80319d
yang <yueyangyu42@gmail.com>
2020-03-06 00:52:59 -0800

    Remove unncessary css styles

--- src/Containers/Explanation.js
+++ src/Containers/Explanation.js
@@ -6,15 +6,9 @@ const Explanation = () => {
   return (
     <Layout stylesClass="column explanation">
       <Text stylesClass="explanation-title" text="How it works!" />
-      <Text
-        stylesClass="explanation-body"
-        text="1. Upload any number of images of anyone with their face clearly in view."
-      />
-      <Text stylesClass="explanation-body" text="2. Generate the emojis." />
-      <Text
-        stylesClass="explanation-body"
-        text="3. Save them all as a zip with one click!"
-      />
+      <Text text="1. Upload any number of images of anyone with their face clearly in view." />
+      <Text text="2. Generate the emojis." />
+      <Text text="3. Save them all as a zip with one click!" />
     </Layout>
   );
 };
