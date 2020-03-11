9f4b80ca8ebc143e586236cd5df4b192cbca2e5f
yang <yueyangyu42@gmail.com>
2020-03-11 12:48:35 -0700

    Remove comments

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-// import express (after npm install express)
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 
-// create new express app and save it as "app"
 const app = express();
 
 app.use(express.json({ extended: true }));
 app.use(cors({ origin: "http://localhost:3000" }));
 
-// server configuration
 const PORT = 5000;
 
-// create a route for the app
 app.post("/getEmojis", async (req, res) => {
   let emojis = [];
   let error = "";
@@ -34,7 +30,6 @@ app.post("/getEmojis", async (req, res) => {
   return res.send({ emojis, error });
 });
 
-// make the server listen to requests
 app.listen(PORT, () => {
   console.log(`Server running at: http://localhost:${PORT}/`);
 });
